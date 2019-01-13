de578ea165430f26794a1d7d2de67ed5b8a17bd1
Yeimer Molina <yeimer.molina@gmail.com>
2019-01-13 16:32:16 -0500

    Add album detail component

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = ({ album }) => (
+  <Card>
+    <CardSection>
+      <View style={styles.thumbnailContainerStyle}>
+        <Image style={styles.thumbnailStyle} source={{ uri: album.thumbnail_image }} />
+      </View>
+      <View style={styles.headerContentStyle}>
+        <Text style={styles.headerTextStyle}>{album.title}</Text>
+        <Text>{album.artist}</Text>
+      </View>
+    </CardSection>
+    <CardSection>
+      <Image style={styles.albumCoverStyle} source={{ uri: album.image }} />
+    </CardSection>
+  </Card>
+);
+
+const styles = {
+  headerContentStyle: {
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumCoverStyle: {
+    width: null,
+    flex: 1,
+    height: 300
+  }
+};
+
+export default AlbumDetail;
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = {
@@ -14,17 +15,15 @@ class AlbumList extends Component {
 
   renderAlbums() {
     return this.state.albums.map(album => (
-      <Text key={album.title}>{album.title}</Text>
+      <AlbumDetail key={album.title} album={album}/>
     ));
   }
 
   render() {
-    console.log(this.state);
     return (
-      <View>
-        <Text>Album lists</Text>
+      <ScrollView>
         {this.renderAlbums()}
-      </View>
+      </ScrollView>
     );
   }
 }
